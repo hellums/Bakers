5c9166983860da9223446cbc708c08d1bf173fa5
hellums <83464025+hellums@users.noreply.github.com>
2022-07-05 10:47:23 -0400

    Add derivative classes and overrides for baker percentages of each bread type

---
+++ bakers/Models/PercentageModel.Sandwich.cs
@@ -0,0 +1,12 @@
+﻿public partial class PercentageModel
+{
+    public class Sandwich : PercentageModel
+    {
+        public override double FlourPercent { get => base.FlourPercent; set => base.FlourPercent = 100.0; }
+        public override double WaterPercent { get => base.WaterPercent; set => base.WaterPercent = 63.0; }
+        public override double YeastPercent { get => base.YeastPercent; set => base.YeastPercent = 1.2; }
+        public override double SaltPercent { get => base.SaltPercent; set => base.SaltPercent = 1.0; }
+        public override double SugarPercent { get => base.SugarPercent; set => base.SugarPercent = 3.5; }
+        public override double OilPercent { get => base.OilPercent; set => base.OilPercent = 8.0; }
+    }
+}
