fa74f03521e32eea2361a4c46efb40528877e902
hellums <83464025+hellums@users.noreply.github.com>
2022-07-05 16:04:51 -0400

    add basic custom logger function

---
+++ bakers/Models/Logger/Logger.cs
@@ -0,0 +1,28 @@
+﻿namespace bakers.Models.Logger
+{
+    public class Logger : LogBase
+    {
+        private string CurrentDirectory { get; set; }
+        private string FileName { get; set; }
+        private string FilePath { get; set; }
+
+    public Logger()
+        {
+            this.CurrentDirectory = Directory.GetCurrentDirectory();
+            this.FileName = "bakersLog.txt";
+            this.FilePath = this.CurrentDirectory + "/" + this.FileName;
+            Console.WriteLine(this.FilePath);
+        }
+
+        public override void Log(string Message)
+        {
+            using (System.IO.StreamWriter w = System.IO.File.AppendText(this.FilePath)) 
+                {
+                    w.WriteLine("————————");
+                    w.Write("\r\nLog Entry: ");
+                    w.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString());
+                    w.WriteLine("  :{0}", Message);
+                }
+        }
+    }
+}
