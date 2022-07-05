9d4b2bb220ab912de918cddb952ca14fddbdb5b1
hellums <83464025+hellums@users.noreply.github.com>
2022-07-05 10:46:34 -0400

    Add derivative classes for each bread type

--- bakers/Models/PercentageModel.cs
+++ bakers/Models/PercentageModel.cs
@@ -1,15 +1,30 @@
-﻿public class PercentageModel : IPercentageModel
+﻿public partial class PercentageModel : IPercentageModel
 {
     public int? Mass { get; set; }
     public string? Bread { get; set; }
     public double FlourAmount { get; set; }
     public double WaterAmount { get; set; }
+    public double YeastAmount { get; set; }
+    public double SaltAmount { get; set; }
+    public double SugarAmount { get; set; }
+    public double OilAmount { get; set; }
+    public virtual double FlourPercent { get; set; } = 100;
+    public virtual double WaterPercent { get; set; }
+    public virtual double YeastPercent { get; set; }
+    public virtual double SaltPercent { get; set; }
+    public virtual double SugarPercent { get; set; }
+    public virtual double OilPercent { get; set; }
     public List<string> breadList { get; set; } = new List<string>
-        { "Ciabatta", "Focaccia", "Sandwich", "Pizza", "Buns" };
+        { "Ciabatta", "Focaccia", "Pizza", "Baguette", "Sandwich", "Buns" };
 
-public void CalculateRatio()
+    public void CalculateRatio()
     {
-        FlourAmount = (double)(Mass * 0.6);
-        WaterAmount = (double)(Mass * 0.4);
+        double TotalPercent = (FlourPercent + WaterPercent + YeastPercent + SaltPercent + SugarPercent + OilPercent) / 100;
+        FlourAmount = (double)(Mass / TotalPercent);
+        WaterAmount = (double)(FlourAmount * (WaterPercent/100));
+        YeastAmount = (double)(WaterAmount * (YeastPercent/100));
+        SaltAmount = (double)(SaltAmount * (SaltPercent/100));
+        SugarAmount = (double)(SugarAmount * (SugarPercent/100));
+        OilAmount = (double)(OilAmount * (OilPercent/100));
     }
 }
