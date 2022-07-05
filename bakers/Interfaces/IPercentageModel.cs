1667b05394920320fe725c86bed5859525eeca3d
hellums <83464025+hellums@users.noreply.github.com>
2022-07-05 10:45:25 -0400

    Add derivative classes for each bread type

--- bakers/Interfaces/IPercentageModel.cs
+++ bakers/Interfaces/IPercentageModel.cs
@@ -4,7 +4,10 @@
     public string? Bread { get; set; }
     public double FlourAmount { get; set; }
     public double WaterAmount { get; set; }
-
+    public double YeastAmount { get; set; }
+    public double SaltAmount { get; set; }
+    public double SugarAmount { get; set; }
+    public double OilAmount { get; set; }
     public void CalculateRatio()
     {
     }
