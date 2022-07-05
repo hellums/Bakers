6207f89c1c1faa69adbe6340eb2fa49b563ce57e
hellums <83464025+hellums@users.noreply.github.com>
2022-07-05 15:11:51 -0400

    change bread type to lower case, move output over a few spaces for readability

--- bakers/Views/PercentageView.cs
+++ bakers/Views/PercentageView.cs
@@ -15,7 +15,7 @@ public class PercentageView : IPercentageView
             {
                 Console.WriteLine("\nAvailable bread types include:");
                 foreach (string? style in breadList) { Console.WriteLine(style); }
-                Console.WriteLine("\nWhat type of dough do you need? (or 0 to exit)");
+                Console.Write("\nWhat type of dough do you need (or 0 to exit)? ");
                 Bread = Console.ReadLine();
                 if (Bread == "0") userSelectedExit = true;
                 if (userSelectedExit) break;
@@ -31,7 +31,7 @@ public class PercentageView : IPercentageView
         bool isInteger = true;
         do
         {
-            Console.WriteLine("\nHow many grams of dough do you need? (or 0 to exit)");
+            Console.Write("\nHow many grams of dough do you need (or 0 to exit)? ");
             isInteger = Int32.TryParse(Console.ReadLine(), out Number);
             if (Number < 0 || !isInteger) Console.WriteLine("\nInvalid amount of dough\n");
         } while ((Number < 0) || !(isInteger));
@@ -39,18 +39,18 @@ public class PercentageView : IPercentageView
     }
     public void ShowResults(IPercentageModel model)
     {
-        Console.WriteLine("\nFor {0} grams of {1}:", model.Mass, model.Bread);
-        Console.WriteLine("---------------------------");
-        Console.WriteLine("flour: {0}", (int)model.FlourAmount);
-        Console.WriteLine("water: {0}", (int)model.WaterAmount);
-        Console.WriteLine("yeast: {0}", (int)model.YeastAmount);
-        if ((int)model.SaltAmount > 0) Console.WriteLine("salt: {0}", (int)model.SaltAmount);
-        if ((int)model.SugarAmount > 0) Console.WriteLine("sugar: {0}", (int)model.SugarAmount);
-        if ((int)model.OilAmount > 0) Console.WriteLine("oil/butter/fat: {0}", (int)model.OilAmount);
+        Console.WriteLine("\n  For {0} grams of {1}:", model.Mass, model.Bread);
+        Console.WriteLine("  ---------------------------");
+        Console.WriteLine("  flour: {0}", (int)model.FlourAmount);
+        Console.WriteLine("  water: {0}", (int)model.WaterAmount);
+        Console.WriteLine("  yeast: {0}", (int)model.YeastAmount);
+        if ((int)model.SaltAmount > 0) Console.WriteLine("  salt: {0}", (int)model.SaltAmount);
+        if ((int)model.SugarAmount > 0) Console.WriteLine("  sugar: {0}", (int)model.SugarAmount);
+        if ((int)model.OilAmount > 0) Console.WriteLine("  oil/butter/fat: {0}", (int)model.OilAmount);
     }
 
     public void Goodbye()
     {
-        Console.WriteLine("\nDon't have a good day. Have a GREAT day!");
+        Console.WriteLine("\n Don't have a good day. Have a GREAT day!");
     }
 }
